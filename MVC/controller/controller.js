43ba5d95f71eb562355646584d6e337af703e1dd
lifetreeandwater <shenhongbinshb@163.com>
2021-04-10 13:16:08 +0800

    update:template

--- MVC/controller/controller.js
+++ MVC/controller/controller.js
@@ -1,3 +1,17 @@
-exports.index =(req,res)=>{
-    res.render('index')
+exports.index = (req, res) => {
+    res.render('index', {
+        title: '秋一布谷鸟',
+        heros: [{
+                name: 'timor',
+                age: 11
+            }, {
+                name: 'mouse',
+                age: 3
+            },
+            {
+                name: 'cat',
+                age: 1
+            }
+        ]
+    })
 }
